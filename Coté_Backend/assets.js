520ffd3376d7017b98ee383661351435fb3090dc
Hasnae387 <hasnaebouchareb387@gmail.com>
2025-01-01 00:18:47 +0100

    changes

---
+++ Coté_Backend/assets.js
@@ -0,0 +1,124 @@
+const menu = [
+  {
+    category: "Entrées",
+    items: [
+      {
+        name: "Briouates",
+        description: "Feuilletés farcis au fromage, viande hachée ou légumes.",
+        price: 20,
+        image: "./img_Menu/Briouates2.jpeg",
+      },
+      {
+        name: "Harira",
+        description: "Soupe traditionnelle parfumée aux épices.",
+        price: 15,
+        image: "./img_Menu/Harira.jpeg",
+      },
+    ],
+  },
+  {
+    category: "Plats Principaux",
+    items: [
+      {
+        name: "Tajine de Poulet",
+        description: "Poulet mijoté avec olives et citron confit.",
+        price: 12,
+        image: "./img_Menu/Tajine de poulet aux olives.jpeg",
+      },
+      {
+        name: "Pastilla au Poulet",
+        description: "Feuilleté sucré-salé au poulet, amandes et cannelle.",
+        price: 14,
+        image: "./img_Menu/Pastilla.jpeg",
+      },
+      {
+        name: "Tajine Kefta",
+        description: "Boulettes de viande hachée en sauce tomate.",
+        price: 14,
+        image: "./img_Menu/keftaTajine.jpeg",
+      },
+      {
+        name: "Tajine d'Agneau aux Pruneaux",
+        description: "Agneau tendre avec pruneaux et amandes.",
+        price: 14,
+        image: "./img_Menu/La recette du tajine d'agneau aux pruneaux anti-inflammatoire ! - BMoove.jpeg",
+      },
+      {
+        name: "Couscous Royal",
+        description: "Semoule fine accompagnée de légumes et viande.",
+        price: 14,
+        image: "./img_Menu/Couscous Tfaya.jpeg",
+      },
+      {
+        name: "Rfissa",
+        description: "Poulet aux lentilles servi sur msemen.",
+        price: 14,
+        image: "./img_Menu/Rfissa2.jpeg",
+      },
+    ],
+  },
+  {
+    category: "Salades",
+    items: [
+      {
+        name: "Salade Marocaine",
+        description: "Tomates, concombres et persil à l'huile d'olive.",
+        price: 12,
+        image: "./img_Menu/Salade marocaine.jpeg",
+      },
+      {
+        name: "Zaalouk",
+        description: "Purée d’aubergines épicée aux tomates.",
+        price: 12,
+        image: "./img_Menu/Zaalouk.jpeg",
+      },
+      {
+        name: "Taktouka",
+        description: "Salade cuite de poivrons et tomates.",
+        price: 14,
+        image: "./img_Menu/Taktouka.jpeg",
+      },
+    ],
+  },
+  {
+    category: "Desserts",
+    items: [
+      {
+        name: "Cornes de Gazelle",
+        description: "Pâtisseries fourrées aux amandes.",
+        price: 12,
+        image: "./img_Menu/Cornes de gazelle .jpeg",
+      },
+      {
+        name: "Chebakia",
+        description: "Pâtisserie au miel et sésame.",
+        price: 14,
+        image: "./img_Menu/Chebakia.jpeg",
+      },
+      {
+        name: "Ghriba",
+        description: "Biscuit sablé fondant aux amandes.",
+        price: 14,
+        image: "./img_Menu/Ghriba2.jpeg",
+      },
+      {
+        name: "Makrout",
+        description: "Gâteau à la semoule et aux dattes.",
+        price: 14,
+        image: "./img_Menu/makrout.jpeg",
+      },
+    ],
+  },
+  {
+    category: "Boissons",
+    items: [
+      {
+        name: "Thé à la Menthe",
+        description: "Thé vert infusé avec de la menthe fraîche.",
+        price: 12,
+        image: "./img_Menu/The3.jpeg",
+      },
+    ],
+  },
+];
+module.exports = { menu };
